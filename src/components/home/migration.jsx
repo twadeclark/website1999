2d82cd3c636d07a90ba80db9155f1ea188985bfa
twadeclark <115502590+twadeclark@users.noreply.github.com>
2023-10-27 19:53:38 -0700

    fixed hidden client side error

--- src/components/home/migration.jsx
+++ src/components/home/migration.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
 import Nav from "react-bootstrap/Nav";
 
-export const Jumbotron = (props) => {
+// export const Jumbotron = (props) => {
+
+//   var bgStyle = props.style || {
+//     backgroundColor: "#e9ecef"
+//   };
+  
+//   return (
+//     <div id={props.id} className={`py-3 ${props.className}`} style={bgStyle}>
+//       <div className="container py-5">
+//         {props.children}
+//       </div>
+//     </div>
+//   );
+// }
+export const Jumbotron = React.forwardRef((props, ref) => {
 
   var bgStyle = props.style || {
     backgroundColor: "#e9ecef"
   };
   
   return (
-    <div id={props.id} className={`py-3 ${props.className}`} style={bgStyle}>
+    <div ref={ref} id={props.id} className={`py-3 ${props.className}`} style={bgStyle}>
       <div className="container py-5">
         {props.children}
       </div>
     </div>
   );
-}
+});
 
 export const NavLink = (props) => {
   return (
